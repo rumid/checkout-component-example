944beb8993ae301d205aebc8b5a757c81c424cbe
Dariusz Orlowski <d.m.orlowski@gmail.com>
2017-08-30 12:11:44 +0200

    Initial commit

---
+++ src/main/java/com/rumid/checkoutcomponentexample/CheckoutComponentExampleApplication.java
@@ -0,0 +1,12 @@
+package com.rumid.checkoutcomponentexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutComponentExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutComponentExampleApplication.class, args);
+	}
+}
